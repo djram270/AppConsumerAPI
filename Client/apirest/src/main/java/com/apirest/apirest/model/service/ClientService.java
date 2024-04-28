976a89951c1301b40ea595faadb8af937ad97d7e
MiguelP0205 <153331902+MiguelP0205@users.noreply.github.com>
2024-04-28 10:56:16 -0500

    microservicio cliente

---
+++ Client/apirest/src/main/java/com/apirest/apirest/model/service/ClientService.java
@@ -0,0 +1,57 @@
+package com.apirest.apirest.model.service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.apirest.apirest.model.entity.Client;
+import com.apirest.apirest.repository.iClientRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ClientService {
+
+    private final  iClientRepository clientRepo;
+
+    public void createClient(Client client)
+    {
+        clientRepo.save(client);
+    }
+
+    public ArrayList<Client> getClients()
+    {
+        return (ArrayList<Client>) clientRepo.findAll();
+    }
+
+    public Optional<Client> getClientById(Integer id)
+    {
+        return clientRepo.findById(id);
+    }
+
+    public Client updateClientById(Client request, Integer id)
+    {
+        Client client = clientRepo.findById(id).get();
+
+        client.setFirstName(request.getFirstName());
+        client.setLastName(request.getLastName());
+        client.setAddress(request.getAddress());
+
+        return client;
+    }
+
+    public Boolean deleteClientById(Integer id)
+    {
+        try
+        {
+            clientRepo.deleteById(id);
+            return true;
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+    }
+}
