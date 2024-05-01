e5586f679df29e128ca1f3e23e8d05aad6e1ba30
mortadela01 <163693837+mortadela01@users.noreply.github.com>
2024-05-01 16:46:49 -0500

    microservicio products

---
+++ MicroserviceProduct/src/main/java/co/unicauca/microservice/apirest/model/service/iCategoryService.java
@@ -0,0 +1,15 @@
+package co.unicauca.microservice.apirest.model.service;
+
+import co.unicauca.microservice.apirest.model.entity.Category;
+import co.unicauca.microservice.apirest.model.entity.Product;
+
+import java.util.List;
+
+public interface iCategoryService {
+    List<Category> getCategories();
+    Category getCategory(Long id);
+    List<Category> getCategoryByName(String name);
+    String deleteCategory(Long id);
+    String postCategory(Category category);
+    String putCategory(Category category, Long id);
+}
